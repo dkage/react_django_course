a1690ec944f99e8eafb7fb71596560acd56bf56c
Danilo Cunha <danilokage@gmail.com>
2022-03-20 22:14:51 -0300

    Add new fields for book model

--- first/demo/models.py
+++ first/demo/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=36)
+    title = models.CharField(max_length=36, blank=False, unique=True)
+    description = models.TextField(max_length=256, blank=True)
 
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=3)
 
+    published = models.DateField(blank=True, null=True)
+    is_published = models.BooleanField(default=False)
+
+    cover = models.ImageField(upload_to='covers/', blank=True)
