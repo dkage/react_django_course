0164215da79720889ff0d943a6996d689135375a
Danilo Cunha <danilokage@gmail.com>
2022-03-21 11:54:16 -0300

    Class views test

---
+++ first/demo/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', first_function),
+    path('class_based', )
+]
--- first/demo/views.py
+++ first/demo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+def first_function(request):
+    return HttpResponse('View test.')
+
+
+class SecondView(View):
+    @staticmethod
+    def get(self, request):
+        return HttpResponse('Another view test.')
