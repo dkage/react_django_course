4dcc935d2e575cd142ab29510991b0918c8d39f2
Danilo Cunha <danilokage@gmail.com>
2022-04-27 23:52:11 -0300

    Add OneToOne Relationship class code

--- first/demo/admin.py
+++ first/demo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import *
 
 # # Register your models here.
 # admin.site.register(Book)
@@ -9,3 +9,8 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description']
     search_fields = ['title']
+
+
+@admin.register(BookNumber)
+class BookNumber(admin.ModelAdmin):
+    list_display = ['id', 'isbn_10', 'isbn_13']
--- first/demo/models.py
+++ first/demo/models.py
@@ -8,6 +8,11 @@ from django.db import models
 #         abstract = True
 
 
+class BookNumber(models.Model):
+    isbn_10 = models.CharField(max_length=10, blank=True)
+    isbn_13 = models.CharField(max_length=13, blank=True)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=36, blank=False, unique=True)
     description = models.TextField(max_length=256, blank=True)
@@ -18,6 +23,8 @@ class Book(models.Model):
     is_published = models.BooleanField(default=False)
 
     cover = models.ImageField(upload_to='covers/', blank=True)
+
+    number = models.OneToOneField(BookNumber, null=True, blank=True, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{str(self.id)} - {self.title}"
