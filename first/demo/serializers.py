8b156b4fc854805d1356be539af29bb6a9d0bb64
Danilo Cunha <danilokage@gmail.com>
2022-04-28 23:52:48 -0300

    Add relationship serializer to json on GET request to list all books

--- first/demo/serializers.py
+++ first/demo/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, BookNumber
+
+
+class BookNumberSerializar(serializers.ModelSerializer):
+    class Meta:
+        model = BookNumber
+        fields = '__all__'
 
 
 class BookSerializer(serializers.ModelSerializer):
+    number = BookNumberSerializar(many=False)
+
     class Meta:
         model = Book
-        exclude = []
+        fields = '__all__'
