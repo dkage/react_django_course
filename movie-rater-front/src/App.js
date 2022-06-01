1a3e58b0fa73718180934f9b20fad1d756974b6a
Danilo Cunha <danilokage@gmail.com>
2022-06-01 02:08:37 -0300

    57. Add basic layout css

--- movie-rater-front/src/App.js
+++ movie-rater-front/src/App.js
@@ -9,6 +9,11 @@ function App() {
             <header className="App-header">
                 <h1>Movie Rater - Web</h1>
             </header>
+                <div className={'Layout'}>
+                    <div><h2>Movie List</h2></div>
+                    <div><h2>Movie Details</h2></div>
+                </div>
+
 
         </div>
     );
