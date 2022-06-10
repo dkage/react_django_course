3cc69a20e2af1fe904fad30cf6a1fd70c9bceff6
Danilo Cunha <danilokage@gmail.com>
2022-06-10 01:41:06 -0300

    77. Login Page - Add login API call, and successfully get uuid token

--- movie-rater-front/src/auth/auth.js
+++ movie-rater-front/src/auth/auth.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import { API } from "../services/api-service";
 
 
 function Auth () {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loginClicked = () => {
+        API.loginUser({username, password})
+            .then( r => console.log(r.token))
+            .catch(error => console.log(error));
+    };
+
     return (
-        <h1>Test auth</h1>
+
+        <React.Fragment>
+
+            <div>
+
+                <h1>Login</h1>
+
+                <label htmlFor="username">Username</label><br/>
+                <input type="text" id='username' name='username' placeholder={'username'}
+                       onChange={ event => setUsername(event.target.value) }/>
+
+                <br/><br/>
+
+                <label htmlFor="password">Password</label><br/>
+                <input id='password' name='password' type={"password"} placeholder={'password'}
+                    onChange={ event => setPassword(event.target.value) }/>
+
+                <br/><br/>
+
+                <button type={'submit'} onClick={loginClicked}>Login</button>
+
+            </div>
+        </React.Fragment>
     )
 }
 
--- movie-rater-front/src/services/api-service.js
+++ movie-rater-front/src/services/api-service.js
@@ -41,5 +41,15 @@ export class API {
     }
 
 
+    static loginUser (credentials) {
+        return fetch(`http://127.0.0.1:8000/auth/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials),
+        }).then(resp => resp.json())
+    }
+
 }
 
